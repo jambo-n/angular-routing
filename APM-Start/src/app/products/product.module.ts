bf3b64c10c741c68e0f495b04036b505f04692b5
Nate Jambo <jambo-n@sbcglobal.net>
2021-08-23 11:40:23 -0500

    commit

--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -12,7 +12,8 @@ import { SharedModule } from '../shared/shared.module';
     SharedModule,
     RouterModule.forChild([
       { path: 'products', component: ProductListComponent},
-      { path: 'productdetail', component: ProductDetailComponent}
+      { path: 'products/:id', component: ProductDetailComponent},
+      { path: 'products/:id/edit', component: ProductEditComponent }
     ])
   ],
   declarations: [
